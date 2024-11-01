3aa2819b6ae20d0d7e6f7d7d8232fae49383100a
tauesp127-alexis <gestionti@taurus.com.mx>
2024-11-01 15:32:41 -0600

    Material sidenav, toolbar e iconos

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MaterialModule } from '../../../material/material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout-page',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MaterialModule,
+    RouterLink
+  ],
   templateUrl: './layout-page.component.html',
   styles: ``
 })
 export class LayoutPageComponent {
+
+  public sidebarItems = [
+    { label: 'Listado', icon: 'label', url: './list' },
+    { label: 'Añadir', icon: 'add', url: './new-hero' },
+    { label: 'Buscar', icon: 'search', url: './search' },
+  ]
 
 }
