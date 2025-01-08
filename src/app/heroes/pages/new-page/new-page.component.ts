ba7fc985641e141c12d6edbcd1f281786fee4e6b
tauesp127-alexis <gestionti@taurus.com.mx>
2025-01-08 10:41:29 -0600

    Pagina para editar, agregar o eliminar

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../material/material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MaterialModule,
+    CommonModule
+  ],
   templateUrl: './new-page.component.html',
   styles: ``
 })
 export class NewPageComponent {
 
+  public publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    },
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+  ]
 }
