9f6d55a6860a268a11bd5c80242fa072992d34c2
tauesp127-alexis <gestionti@taurus.com.mx>
2025-01-08 10:56:27 -0600

    CRUD endpoints

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -28,6 +28,24 @@ export class HeroesService {
 
   getSuggestions( query: string ): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseURL}/heroes?q=${query}&_limit=6`)
+  }
+
+  addHero( hero: Hero ): Observable<Hero> {
+    return this.http.post<Hero>(`${ this.baseURL }/heroes`, hero );
+  }
+
+  updateHero( hero: Hero ): Observable<Hero> {
+    if( !hero.id ) throw new Error('Hero ID is required!')
+    return this.http.patch<Hero>(`${ this.baseURL }/heroes/${ hero.id }`, hero );
+  }
+
+  deleteHero( hero: Hero ): Observable<boolean> {
+    
+    return this.http.delete(`${ this.baseURL }/heroes/${ hero.id }`)
+            .pipe(
+              catchError( err => of(false)),
+              map( resp => true)
+            )
   }
   
 }
