074280099cfd2fee0edd46da3029b19c599fa129
tauesp127-alexis <gestionti@taurus.com.mx>
2025-01-08 12:11:52 -0600

    HeroForms

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,18 +1,34 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../../material/material.module';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { HeroImagePipe } from '../../pipes/hero-image.pipe';
 
 @Component({
   selector: 'app-new-page',
   standalone: true,
   imports: [
     MaterialModule,
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    HeroImagePipe
   ],
   templateUrl: './new-page.component.html',
   styles: ``
 })
 export class NewPageComponent {
+
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl('')
+  });
 
   public publishers = [
     {
@@ -28,4 +44,17 @@ export class NewPageComponent {
       desc: 'DC - Comics'
     },
   ]
+
+  constructor( public heroesService: HeroesService ) {}
+
+  get currentHero(): Hero {
+    const hero =  this.heroForm.value as Hero;
+    return hero;
+  }
+
+  onSubmit() {
+    if( this.heroForm.invalid ) return;
+
+    // this.heroesService.updateHero( this.heroForm.value )
+  }
 }
